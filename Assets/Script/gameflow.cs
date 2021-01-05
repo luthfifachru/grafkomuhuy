bd419b11c4ef645594d9d549bcda1d56cd362b06
luthfi.fachru <luthfi.fachru@gmail.com>
2021-01-05 23:18:31 +0700

    arbiter_assets_upload

---
+++ Assets/Script/gameflow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameflow : MonoBehaviour
+{
+    public Transform tile1Obj;
+    private Vector3 nextTileSpawn;  
+
+    void Start()
+    {
+     	nextTileSpawn.z = -84;
+    	StartCoroutine(spawnTile());
+    }
+
+  
+    void Update()
+    {
+
+    }
+    	IEnumerator spawnTile()
+    	{
+    		yield return new WaitForSeconds(1);
+    		Instantiate(tile1Obj, nextTileSpawn, tile1Obj.rotation);
+    		nextTileSpawn.z += -7;
+    		Instantiate(tile1Obj, nextTileSpawn, tile1Obj.rotation);
+    		nextTileSpawn.z += -7;
+    		StartCoroutine(spawnTile());
+    	}
+}
+
